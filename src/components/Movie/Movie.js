adaae5d4c49078b00adae4d36363bd4cf7530eca
ichanddan <code.us.cm@gmail.com>
2024-03-29 14:01:04 +0530

    Add funcality home page

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,22 +1,43 @@
-import React from "react";
-import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import React, { useState } from "react";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import ReactStars from "react-rating-stars-component";
 
 export default function Movie() {
+  const [movieData, setMoviedata] = useState([
+    {
+      title: "Avengers Endgame 4k",
+      year: 2018,
+      rating: 3,
+      poster:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQA_-tL18_rj9zEcjN6n41NEaJm-kRNF9UeOtvksZ4z_OW6jRA9",
+    },
+    {
+      title: "Avengers Endgame 4k",
+      year: 2018,
+      rating: 4.5,
+      poster:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQA_-tL18_rj9zEcjN6n41NEaJm-kRNF9UeOtvksZ4z_OW6jRA9",
+    },
+  ]);
   return (
-    <Card className="mt-5 mx-5 w-1/5">
-      <CardBody className="overflow-visible py-2">
-        <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
-          src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQA_-tL18_rj9zEcjN6n41NEaJm-kRNF9UeOtvksZ4z_OW6jRA9"
-          width={270}
-        />
-      </CardBody>
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <h1 className="font-bold text-large">Frontend Radio</h1>
-        <p className="text-tiny uppercase font-bold">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small>
-      </CardHeader>
-    </Card>
+    <div className="md:flex flex-wrap items-center justify-center my-10 ">
+      {movieData.map((element, index) => {
+        return (
+          <Card key={index} className="mt-5 mx-5 w-1/5">
+            <CardBody className="overflow-visible py-2">
+              <Image
+                alt="Card background"
+                className="object-cover rounded-xl"
+                src={element.poster}
+                width={270}
+              />
+            </CardBody>
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+              <h1 className="font-bold text-large">{element.title}</h1>
+              <ReactStars half={true} size={20} value={element.rating} edit={false}/>              
+              <p className="text-tiny uppercase font-bold">{element.year}</p>
+            </CardHeader>
+          </Card>
+        );
+      })}
+    </div>
   );
 }
