c35f8ae207dd161a5a13a55af2b10d98ee8a4721
ichanddan <code.us.cm@gmail.com>
2024-03-29 14:36:52 +0530

    add funcationality add move section

--- src/components/Movie/AddMovie.js
+++ src/components/Movie/AddMovie.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddMovie = () => {
+    const [dataStore, setDatastore] = useState({
+        title:"",
+        image:"",
+        year:Number,
+        description:""
+    })
+    const [store, setStore]= useState([])
   return (
     <>
       <div className="max-w-md mx-auto my-16">
         <div className="relative z-0 w-full mb-5 group">
           <input
-            type="email"
+          value={dataStore.title}
+          onChange={(e)=>{setDatastore({...dataStore, title: e.target.value})}}
+            type="text"
             name="floating_email"
             id="floating_email"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -22,7 +31,9 @@ export const AddMovie = () => {
         </div>
         <div className="relative z-0 w-full mb-5 group">
           <input
-            type="password"
+          value={dataStore.image}
+          onChange={(e)=>{setDatastore({...dataStore, image: e.target.value})}}
+            type="text"
             name="floating_password"
             id="floating_password"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -38,7 +49,9 @@ export const AddMovie = () => {
         </div>
         <div className="relative z-0 w-full mb-5 group">
           <input
-            type="password"
+          value={dataStore.year}
+          onChange={(e)=>{setDatastore({...dataStore, year: e.target.value})}}
+            type="number"
             name="repeat_password"
             id="floating_repeat_password"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -55,6 +68,8 @@ export const AddMovie = () => {
             Description
           </label>
           <textarea
+          value={dataStore.description}
+          onChange={(e)=>{setDatastore({...dataStore, description: e.target.value})}}
             type="text"
             className="h-36 block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             required
@@ -62,6 +77,7 @@ export const AddMovie = () => {
         </div>
         <div className="flex items-center justify-center">
         <button
+        onClick={()=>{setStore([...store,{dataStore}])}}
           type="submit"
           className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
