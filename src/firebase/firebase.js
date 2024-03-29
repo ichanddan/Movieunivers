17aaa49182133a48afc805ae82a485b7ebf8e65a
ichanddan <code.us.cm@gmail.com>
2024-03-29 14:41:23 +0530

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCIxpLT2_mfKz98Ml8S0uRqspSVBPtLEw",
+  authDomain: "movieunivers-154ff.firebaseapp.com",
+  projectId: "movieunivers-154ff",
+  storageBucket: "movieunivers-154ff.appspot.com",
+  messagingSenderId: "271174305448",
+  appId: "1:271174305448:web:bef79ec8126571b58e249d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
